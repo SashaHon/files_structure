9d8db29def852ae0aad77ed246c4bafcfa13866c
Oleksandra (SASHA) Honchar <88204002+SashaHon@users.noreply.github.com>
2026-02-27 12:21:44 +0100

    wip: work on api algorithm

--- server.ts
+++ server.ts
@@ -1,17 +1,76 @@
 import express from "express";
 import cors from "cors";
-import moviesData from "./data/movies";
+import data from "./data/data.ts";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const ROOT_NODE_ID = "root";
+
+type ItemType = "folder" | "file";
+
+type Node = {
+  id?: string;
+  type: ItemType;
+  children?: Record<string, Node>;
+};
+
+type FlatNode = {
+  id: string;
+  type: ItemType;
+};
+
+type resultFlatNodes = {
+  flatNodes: Array<FlatNode> | null;
+  depthLevel: number;
+} | null;
+
+function getNodeById(node: Node, id: string): Node | null {
+  if (!node) return null;
+
+  let result: resultFlatNodes = null;
+  let flatNodes: Array<FlatNode> = [];
+  let depthLevel = 0;
+
+  if (id === ROOT_NODE_ID) {
+    flatNodes = Object.entries(node).map(([key, value]) => ({
+      id: key,
+      type: value.type,
+    }));
+    depthLevel = 1;
+
+    result = {
+      flatNodes,
+      depthLevel,
+    };
+
+    console.log("ROOT NODE: !!XX!! ", result);
+    return result;
+  }
+
+  for (const key in node) {
+    if (key === id) {
+      return node[key];
+    }
+  }
+  return "cannot find node by id";
+}
+
 app.get("/", (req, res) => {
-  res.send("Server is running. Try /api/movies");
+  res.send("Server is running. Try /api/data");
 });
 
-app.get("/api/movies", (req, res) => {
-  res.json(moviesData);
+app.get("/api/data", (req, res) => {
+  res.json(data);
+});
+
+app.get("/api/data/:id", (req, res) => {
+  const { id } = req.params;
+
+  const node = getNodeById(data.root, id);
+  console.log("NODE: ", node);
+  res.json(node?.flatNodes ?? []);
 });
 
 app.listen(3001, () => {
--- src/components/Main/Screen.vue
+++ src/components/Main/Screen.vue
@@ -1,15 +1,18 @@
 <template>
   <h1>Entertainment</h1>
   <ul>
-    <li>Movies</li>
-    <li>Comics</li>
-    <li>{{ data }}</li>
+    <li>ouihop</li>
+    <li>{{ rootElement }}</li>
+    <li>3</li>
   </ul>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useEntertainmentData } from "../../composables/useEntertainmentData";
 
 const { data, isLoading, error, fetchEntertainmentData } =
   useEntertainmentData();
+
+const rootElement = computed(() => data.value ?? null);
 </script>
